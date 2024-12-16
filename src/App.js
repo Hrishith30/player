cc3827fe39c3e14324535554c7c7d59248d5f310
Hrishith30 <rishi6211130@gmail.com>
2024-12-16 09:09:35 +0530

    music

--- src/App.js
+++ src/App.js
@@ -297,14 +297,7 @@ function App() {
 
     // Handle visibility change
     const handleVisibilityChange = () => {
-      if (navigator.userAgent.match(/(iPad|iPhone|iPod)/i)) {
-        // For iOS, mute the audio when the app goes to the background
-        if (document.hidden) {
-          audio.muted = true;
-        } else {
-          audio.muted = false;
-        }
-      } else if (document.hidden && isPlaying) {
+      if (document.hidden && isPlaying) {
         audio.play().catch(error => console.error('Playback failed:', error));
       }
     };
